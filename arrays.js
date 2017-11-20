c373ed7a4ced204a2854f269a197f159638c0827
John Corujo <corujojohn@gmail.com>
2017-11-20 10:14:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function removeElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars.slice(1);
 }
 
-function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+function destructivelyRemoveElementbeginingOfArray(chocolateBars) {
   return chocolateBars.pop(1);
 }
