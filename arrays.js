891be7edc98a2b342250f553ef94bd59c2ddff80
John Corujo <corujojohn@gmail.com>
2017-11-20 10:04:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,4 +22,6 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-f
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
