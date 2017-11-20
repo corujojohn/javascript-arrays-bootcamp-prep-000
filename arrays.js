4bd9cb1e4f528eaec51bf0fc1ac4110cf916a946
John Corujo <corujojohn@gmail.com>
2017-11-20 10:00:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,23 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.unshift(element);
-  return array
+function destructivelyAddElementToBeginningOfArray(array, element) {
+   array.unshift(element);
+   return array
+}
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+   array.push(element)
+   return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
